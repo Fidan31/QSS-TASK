079297c8c0fc86eb17b84084b9ad75396f3ce24f
Fidan Mammadova <imfidanmamedova@gmail.com>
2024-01-26 17:22:53 +0400

    first commit

---
+++ src/components/Barchart.jsx
@@ -0,0 +1,84 @@
+import { useState } from "react";
+import "./style/Barchart.css";
+
+const Barchart = ({ barchart, indicator = [], searchParams }) => {
+  const [overMouse, setOverMouse] = useState(false);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [overMouseItem, setOverMouseItem] = useState(null);
+  const indicatorParam =
+    // eslint-disable-next-line react/prop-types
+    searchParams.get("indicator") || (indicator.length > 0 ? indicator[0] : "");
+
+  const handleMouseOver = (e, item) => {
+    setOverMouse(true);
+    setOverMouseItem(item);
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
+
+  const handleMouseOut = () => {
+    setOverMouse(false);
+    setOverMouseItem(null);
+  };
+
+  const max = barchart?.max_amount;
+  return (
+    <div className="order-container">
+      <div className="indicator-headding">{indicatorParam}</div>
+      <div className="order-listing-container">
+        <div className="listing-headding">
+          <h3>Rank</h3>
+          <h3>Country</h3>
+        </div>
+        <ul className="country-data-lists">
+          {barchart?.countries_by_rank?.map((item, index) => (
+            <li className="country-data-list" key={index}>
+              <div className="little-cont">
+                <span className="order-number">{item?.rank}</span>
+                <div className="country-and-flag">
+                  <span className="country-name">{item?.country}</span>
+                  <img className="flags"
+                    src={`https://flagcdn.com/${item?.country_code_2.toLowerCase()}.svg`}
+                    alt=""
+                  />
+                </div>
+              </div>
+
+              <div
+                className="line-container"
+                onMouseOver={(e) => handleMouseOver(e, item)}
+                onMouseOut={handleMouseOut}
+              >
+                {overMouse && (
+                  <div
+                    style={{
+                      position: "fixed",
+                      left: position.x + 10,
+                      top: position.y + 10,
+                      background: "rgba(255, 255, 255, 0.8)",
+                      padding: "10px",
+                      width: "200px",
+                      borderRadius: "5px",
+                      zIndex: "4",
+                    }}
+                  >
+                    <p>{overMouseItem?.country}</p>
+                    <p>{overMouseItem?.year}</p>
+                    <p>{overMouseItem?.rank}</p>
+                  </div>
+                )}
+                <div
+                  className="line"
+                  style={{
+                    width: `${(item?.amount / max) * 100}%`,
+                  }}
+                ></div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Barchart;
---
+++ src/pages/home/index.jsx
@@ -0,0 +1,172 @@
+import { useCallback, useEffect, useState } from "react";
+import Select from "../../components/Select";
+import axios from "axios";
+import { useSearchParams } from "react-router-dom";
+import "./style.css";
+import Country from "../../components/Country";
+import Rank from "../../components/Rank";
+import Barchart from "../../components/Barchart";
+
+
+
+const Home = () => {
+  const BASE_URL = import.meta.env.VITE_APP_BASE_URL;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const [sectors, setSectors] = useState([]);
+  const [subSectors, setSubSectors] = useState([]);
+  const [indicator, setIndicator] = useState([]);
+  const [country, setCountry] = useState([]);
+  const [years, setYears] = useState([]);
+  const [sliderValue, setSliderValue] = useState([20, 50]);
+  const [barChart, setBarChart] = useState([]);
+
+  console.log(barChart, "ch");
+  const fetchData = async (url, setState, params = {}) => {
+    try {
+      const response = await axios.get(url, { params });
+      setState(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchData(`${BASE_URL}/sectors`, setSectors);
+  }, [BASE_URL]);
+
+  useEffect(() => {
+    const sectorParam =
+      searchParams.get("sector") || (sectors.length > 0 ? sectors[0] : "");
+
+    fetchData(`${BASE_URL}/subsectors`, setSubSectors, { sector: sectorParam });
+  }, [BASE_URL, searchParams, sectors]);
+
+  useEffect(() => {
+    const subSectorParam =
+      searchParams.get("subsector") ||
+      (subSectors.length > 0 ? subSectors[0] : "");
+
+    fetchData(`${BASE_URL}/indicators`, setIndicator, {
+      subsector: subSectorParam,
+    });
+  }, [BASE_URL, searchParams, subSectors]);
+
+  useEffect(() => {
+    const indicatorParam =
+      searchParams.get("indicator") ||
+      (indicator.length > 0 ? indicator[0] : "");
+
+    fetchData(`${BASE_URL}/country`, setCountry, {
+      indicator: indicatorParam,
+    });
+  }, [BASE_URL, indicator, searchParams]);
+
+  useEffect(() => {
+    const indicatorParam =
+      searchParams.get("indicator") ||
+      (indicator.length > 0 ? indicator[0] : "");
+
+    const countryParam =
+      searchParams.get("country") || (country.length > 0 ? country[0] : "");
+    console.log(indicatorParam.replace(" ", ";"));
+    fetchData(`${BASE_URL}/years`, setYears, {
+      countries: countryParam.replaceAll(",", ";"),
+      indicator: indicatorParam,
+    });
+  }, [BASE_URL, indicator, country, searchParams]);
+
+  useEffect(() => {
+    const indicatorParam =
+      searchParams.get("indicator") ||
+      (indicator.length > 0 ? indicator[0] : "");
+
+    const yearsParams =
+      searchParams.get("year") || (years.length > 0 ? years[0] : "");
+
+    const countryParam =
+      searchParams.get("country") || (country.length > 0 ? country[0] : "");
+    if (indicatorParam && yearsParams && countryParam) {
+      fetchData(`${BASE_URL}/bar-chart`, setBarChart, {
+        countries: countryParam.replaceAll(",", ";"),
+        indicator: indicatorParam,
+        ranks: sliderValue[0] + "," + sliderValue[1],
+        year1: yearsParams,
+      });
+    }
+  }, [BASE_URL, country, indicator, searchParams, sliderValue, years]);
+
+  const changeQueryParams = useCallback(
+    (key, value) => {
+      searchParams.set(key, value);
+      setSearchParams(searchParams);
+    },
+    [searchParams, setSearchParams]
+  );
+
+  const handleSelectChange = (key, value) => {
+    changeQueryParams(key, value);
+  };
+
+  const changeQueryParamsCountry = useCallback(
+    (key, value) => {
+      searchParams.set(key, value);
+      setSearchParams(searchParams);
+    },
+    [searchParams, setSearchParams]
+  );
+
+  const handleSelectChangeCountry = (key, value) => {
+    changeQueryParamsCountry(key, value);
+  };
+
+  return (
+    <div className="home-container">
+      <div className="filter-wrapper">
+        <Country
+          label="Country"
+          id="country"
+          data={country}
+          onChange={(value) => handleSelectChangeCountry("country", value)}
+          value={searchParams.get("country") || ""}
+        />
+        <Select
+          label="Years"
+          id="years"
+          data={years}
+          onChange={(value) => handleSelectChange("year", value)}
+          value={searchParams.get("year") || ""}
+        />
+        <Select
+          label="Sector"
+          id="sector"
+          data={sectors}
+          onChange={(value) => handleSelectChange("sector", value)}
+          value={searchParams.get("sector") || ""}
+        />
+        <Select
+          label="Subsector"
+          id="subsector"
+          data={subSectors}
+          onChange={(value) => handleSelectChange("subsector", value)}
+          value={searchParams.get("subsector") || ""}
+        />
+        <Select
+          label="Indicator"
+          id="indicator"
+          data={indicator}
+          onChange={(value) => handleSelectChange("indicator", value)}
+          value={searchParams.get("indicator") || ""}
+        />
+
+        <Rank sliderValue={sliderValue} setSliderValue={setSliderValue} />
+      </div>
+      <Barchart
+        barchart={barChart}
+        indicator={indicator}
+        searchParams={searchParams}
+      />
+    </div>
+  );
+};
+
+export default Home;
